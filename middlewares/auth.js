9db2129296eb1b832165f334ba2d74b6df80b48e
Robert Anisimov <skillup23@yandex.ru>
2021-07-10 13:56:38 +0300

    Дипломная. Бэкенд 1.01

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return res
       .status(401)
-      .send({ message: 'Необходима авторизация' });
+      .send({ message: 'Необходима авторизация!' });
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -18,7 +18,7 @@ module.exports = (req, res, next) => {
   } catch (err) {
     return res
       .status(401)
-      .send({ message: 'Необходима авторизация' });
+      .send({ message: 'Необходима авторизация!' });
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
